a1c34770f81fab8b2392cb2b2943687b46d40c83
cyberpunk922 <vermaarun922@gmail.com>
2017-08-13 03:38:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-/*var pool = require('pg').Pool;
+var pool = require('pg').Pool;
 
 
 var config = {
@@ -11,7 +11,7 @@ var config = {
     port: '5432',
     password: process.env.DB_PASSWORD
 };
-*/
+
 var app = express();
 app.use(morgan('combined'));
 
