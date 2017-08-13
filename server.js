125981643617059410172a39480ee709f2d210a2
cyberpunk922 <vermaarun922@gmail.com>
2017-08-13 13:43:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-var pool = require('pg').Pool;
-
+var Pool = require('pg').Pool;
+var crypto = require('crypto');
+var bodyParser = require('body-parser');
+var session = require('express-session');
 
 var config = {
-    user:'vermaarun922',
-    database: 'vermaarun922',
+    user: 'coco98',
+    database: 'coco98',
     host: 'db.imad.hasura-app.io',
     port: '5432',
     password: process.env.DB_PASSWORD
